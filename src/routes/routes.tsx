8c87ae1f2489aa4c69b75615684b1f2a280aa1b2
Toxxxy <tonyxxxq@gmail.com>
2023-09-17 22:39:12 +0300

    added routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes as ReactRouterRoutes, Route } from 'react-router-dom';
+import { HomePage } from '../pages/home.page';
+import { LoginPage } from '../pages/login.page';
+import { SignupPage } from '../pages/signup.page';
+
+const Routes: React.FC = () => {
+  return (
+    <ReactRouterRoutes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="signup" element={<SignupPage />} />
+    </ReactRouterRoutes>
+  )
+}
+
+export { Routes };
