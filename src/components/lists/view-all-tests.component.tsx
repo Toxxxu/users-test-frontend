952971f34ff286bc54238b4ef1058a13c05ff192
Toxxxy <tonyxxxq@gmail.com>
2023-09-18 19:18:46 +0300

    refactor view all tests

--- src/components/lists/view-all-tests.component.tsx
+++ src/components/lists/view-all-tests.component.tsx
@@ -7,34 +7,38 @@ const ViewAllTests: React.FC = () => {
   const tests = getData.tests;
   const loading = getData.loading;
 
+  if (loading) {
+    return (
+      <Grid container alignItems="center" justifyContent="center" style={{ minHeight: 'calc(100vh - 64px)' }}>
+        <Box>
+          <CircularProgress />
+        </Box>
+      </Grid>
+    );
+  }
+
+  if (tests.length === 0) {
+    return null;
+  }
+
   return (
-    <div>
-      {loading ? (
-        <Grid container alignItems="center" justifyContent="center" style={{ minHeight: 'calc(100vh - 64px)' }}>
-          <Box>
-            <CircularProgress />
-          </Box>
-        </Grid>
-      ) : (
-        <>
-          <Typography variant="h4" align="center" gutterBottom>
-            Tests created by users
-          </Typography><Grid container spacing={2}>
-            {tests.map((test) => (
-              <Grid item key={test._id} xs={12} sm={6} md={4}>
-                <Card>
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {test.title}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
+    <>
+      <Typography variant="h4" align="center" gutterBottom>
+        Tests created by users
+      </Typography><Grid container spacing={2}>
+        {tests.map((test) => (
+          <Grid item key={test._id} xs={12} sm={6} md={4}>
+            <Card>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {test.title}
+                </Typography>
+              </CardContent>
+            </Card>
           </Grid>
-        </>
-      )}
-    </div>
+        ))}
+      </Grid>
+    </>
   )
 }
 
